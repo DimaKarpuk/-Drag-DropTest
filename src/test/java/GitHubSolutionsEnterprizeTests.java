6ec59617ac6248bccb095593bd21c7fd4b2cffde
DimaKarpuk <99805543+DimaKarpuk@users.noreply.github.com>
2024-04-24 21:49:19 +0300

    check Solutions

---
+++ src/test/java/GitHubSolutionsEnterprizeTests.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class GitHubSolutionsEnterprizeTests {
+    @BeforeAll
+    static void BeforeAll(){
+        Configuration.browserSize = "1928x1080";  // меняем размер окна браузера
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.holdBrowserOpen = true;
+
+    }
+    @Test
+    void GitSolutionsEnterprise(){
+        open("https://github.com");
+        //На главной странице GitHub выберите меню Solutions.
+        $(Selectors.byTagAndText("button","Solutions")).hover();
+        //Solutions -> Enterprize видимый элемент.
+        $(Selectors.byTagAndText("a","Enterprise")).shouldBe(visible);
+    }
+}
