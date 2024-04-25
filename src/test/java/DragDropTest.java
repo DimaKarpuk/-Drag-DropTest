833c5f6d5a2782d54d02dada7d8f4652013b8f7e
DimaKarpuk <99805543+DimaKarpuk@users.noreply.github.com>
2024-04-25 15:18:36 +0300

    added SecondMethod

--- src/test/java/DragDropTest.java
+++ src/test/java/DragDropTest.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragDropTest {
@@ -9,14 +12,26 @@ public class DragDropTest {
     static void BeforeAll(){
         Configuration.browserSize = "1928x1080";  // меняем размер окна браузера
         Configuration.baseUrl = "https://the-internet.herokuapp.com/";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
 
     }
     @Test
     void DragAndDrop(){
         open("drag_and_drop");
+        $(".example").should(appear);
 
         actions().moveToElement($("#column-a")).clickAndHold()
                 .moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("b"));
+        $("#column-b").shouldHave(text("a"));
+    }
+    @Test
+    void SecondMethod(){
+        open("drag_and_drop");
+        $(".example").shouldHave(text("Drag and Drop"));
+
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-a").shouldHave(text("b"));
+        $("#column-b").shouldHave(text("a"));
     }
 }
